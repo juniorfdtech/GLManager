3461977f9d99828cdcbf1ec7e0ed8951b50234a5
DuTra01 <glemyson20@gmail.com>
2022-06-09 01:23:06 -0300

    update

--- app/data/config/db_config.py
+++ app/data/config/db_config.py
@@ -1,8 +1,25 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_PATH = '/etc/GLManager/'
+DATABASE_NAME = 'db.sqlite3'
+
+if not os.path.exists(DATABASE_PATH):
+    try:
+        os.makedirs(DATABASE_PATH)
+    except:
+        DATABASE_PATH = os.path.join(os.path.expanduser('~'), 'GLManager')
+        if not os.path.exists(DATABASE_PATH):
+            os.makedirs(DATABASE_PATH)
+
+
+DATABASE_URI = 'sqlite:///' + os.path.join(DATABASE_PATH, DATABASE_NAME)
+
+
 class DBConnection:
-    def __init__(self, uri: str = 'sqlite:///db.sqlite3') -> None:
+    def __init__(self, uri: str = DATABASE_URI):
         self.__uri = uri
         self.__engine = create_engine(self.__uri)
         self.__session = None
